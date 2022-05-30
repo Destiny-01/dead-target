99d428238a9b0e5d0e2fa599829d0c7e982a1854
Aigbe <aigbedestinyic@gmail.com>
2022-05-30 01:53:25 +0100

    Add mainnet support

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,9 +4,13 @@ const snarkjs = require("snarkjs");
 const { buildPoseidon } = require("circomlibjs");
 const io = require("socket.io")(app, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: [
+      "https://dead-target.netlify.app",
+      "https://dead-target-testnet.netlify.app",
+      "http://localhost:3000",
+    ],
     methods: ["GET", "POST"],
-    // credentials: true,
+    credentials: true,
   },
 });
 const { initGame } = require("./game");
